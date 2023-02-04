3448603af48b1e9e30b0d6633c3b7a60f1f8bc2c
Zeptosec <armandasmatkevicius@gmail.com>
2023-02-04 22:17:45 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,72 @@
+import express from 'express';
+const app = express();
+import stream from 'stream';
+import { getStreamBuffer } from './manager.js';
+
+const fileLimit = 8 * 1024 ** 2;
+const chunkSize = 2 * 1024 ** 2;
+
+app.get("/test/:id", async function (req, res) {
+    try {
+        const { id } = req.params;
+        const rs = await getStreamBuffer(id, 1);
+        console.log(rs);
+    } catch (err) {
+        console.log(err.message);
+    }
+    return res.status(200).json({ ares: "done" });
+
+})
+
+app.get("/stream/:id", async function (req, res) {
+    const range = req.headers.range;
+    const id = req.params.id;
+    if (!id) {
+        return res.status(400).send("No id was specified");
+    }
+    if (!range) {
+        return res.status(400).send("Requires Range header");
+    }
+
+    let canceled = false;
+    req.on('close', () => {
+        canceled = true;
+    })
+    const start = Number(range.replace(/\D/g, ""));
+    try {
+        await new Promise(r => setTimeout(r, 50)); // accounting for sliding of video or audio
+        if (canceled)
+            return res.status(200);
+        const data = await getStreamBuffer(id, start);
+        if (!data.buffer || !data.streamSize) return res.status(500).json({ error: "Could not get stream buffer" });
+
+        const CHUNK_SIZE = Math.min(chunkSize, fileLimit - start % fileLimit - 1);
+        const end = Math.min(start + CHUNK_SIZE, data.streamSize - 1);
+        const contentLength = end - start + 1;
+
+        // i guess i dont need content type
+        // const type = req.headers['sec-fetch-dest'] === 'audio' ? 'audio/mp3' : 'video/mp4';
+        const headers = {
+            "Content-Range": `bytes ${start}-${end}/${data.streamSize}`,
+            "Accept-Ranges": "bytes",
+            "Content-Length": contentLength,
+            // "Content-Type": type,
+            "Access-Control-Allow-Origin": "*"
+        };
+        res.writeHead(206, headers);
+
+        const fend = end % fileLimit == 0 ? start % fileLimit + CHUNK_SIZE : end % fileLimit + 1;
+        const sliced = data.buffer.slice(start % fileLimit, fend);
+
+        var bufferStream = new stream.PassThrough();
+        bufferStream.end(sliced);
+        bufferStream.pipe(res)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: err.message });
+    }
+})
+
+app.listen(process.env.PORT || 8000, function () {
+    console.log("Listening on " + (process.env.PORT || 8000));
+})
---
+++ manager.js
@@ -0,0 +1,69 @@
+import axios from "axios";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// --------- CONFIG -------------
+const maxStreamInfoSize = 20; // max amount of streamsInfo. This does not take much space so this can be fairly high.
+const maxQueueSize = 6; // queue size this is expensive. one item in queue is 8MB. Be careful not to run out of memory.
+// ------------------------------
+const linkStart = process.env.LINKSTART;
+const streamsInfo = new Map();
+const chunkSize = 8 * 1024 ** 2;
+
+async function getStreamInfo(id) {
+    if (streamsInfo.has(id)) {
+        return streamsInfo.get(id);
+    } else {
+        try {
+            const res = await axios.get(`${linkStart}/${id}/blob`);
+            if (!res.data.chunks || !res.data.size) return null;
+            streamsInfo.set(id, res.data);
+            if (streamsInfo.size > maxStreamInfoSize) {
+                streamsInfo.delete(streamsInfo.keys().next().value);
+            }
+            return res.data;
+        } catch (err) {
+            console.log(err);
+            return null;
+        }
+    }
+}
+
+export async function getStreamBuffer(id, start) {
+    const streamInfo = await getStreamInfo(id);
+    if (streamInfo === null) return null;
+    if (start < 0) return null;
+    const index = Math.floor(start / chunkSize);
+    if (index >= streamInfo.chunks.length) return null;
+
+    AddToQueue(id, index, streamInfo);
+    if (index + 1 < streamInfo.chunks.length) // buffer up more stream
+        AddToQueue(id, index + 1, streamInfo);
+
+    return {
+        buffer: (await getFromQueue(id, index)).data,
+        streamSize: streamInfo.size
+    };
+}
+
+let Queue = [];
+function AddToQueue(id, index, streamInfo) {
+    const item = Queue.find(w => w.id === id && w.index === index);
+    if (item) return;
+    const controller = new AbortController();
+    const buffer = axios.get(`${linkStart}/${streamInfo.chunks[index]}/blob`, {
+        responseType: 'arraybuffer',
+        signal: controller.signal
+    }).catch(err => { console.log("canceled"); });
+    Queue.unshift({ id, index, buffer, controller });
+    if (Queue.length > maxQueueSize) {
+        Queue.pop().controller.abort();
+    }
+}
+
+async function getFromQueue(id, index) {
+    const item = Queue.find(w => w.id === id && w.index === index);
+    if (!item) return null;
+    const buffer = await item.buffer;
+    return buffer;
+}
