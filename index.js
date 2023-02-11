165d142760219a293ba6e76ba5bddc8bb19a5987
Zeptosec <armandasmatkevicius@gmail.com>
2023-02-11 14:13:39 +0200

    Fixed undefined when cancelled

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ app.get("/", function (req, res) {
     res.status(200).send("alive");
 })
 
-app.get("/stream/:id", async function (req, res) {
+app.get("/stream/:cid/:fid", async function (req, res) {
     const range = req.headers.range;
-    const id = req.params.id;
-    if (!id) {
-        return res.status(400).send("No id was specified");
+    const { fid, cid } = req.params;
+    if(cid.length !== 19 || fid.length !== 19){
+        return res.status(400).send("Ids are not valid.");
+    }
+    if (!cid || !fid) {
+        return res.status(400).send("No fid or cid was specified");
     }
     if (!range) {
         return res.status(400).send("Requires Range header");
     }
-
     let canceled = false;
     req.on('close', () => {
         canceled = true;
@@ -26,14 +28,14 @@ app.get("/stream/:id", async function (req, res) {
         await new Promise(r => setTimeout(r, 50)); // accounting for sliding of video or audio
         if (canceled)
             return res.status(200);
-        const data = await getStreamBuffer(id, start);
-        if (!data.buffer || !data.streamSize) return res.status(500).json({ error: "Could not get stream buffer" });
+        const data = await getStreamBuffer(fid, cid, start);
+        if (data === null || !data.buffer || !data.streamSize) return res.status(500).json({ error: "Could not get stream buffer" });
         const chunkSize = data.chunkSize / 4;
         const fileLimit = data.chunkSize;
+
         const CHUNK_SIZE = Math.min(chunkSize, fileLimit - start % fileLimit - 1);
         const end = Math.min(start + CHUNK_SIZE, data.streamSize - 1);
         const contentLength = end - start + 1;
-
         // i guess i dont need content type
         // const type = req.headers['sec-fetch-dest'] === 'audio' ? 'audio/mp3' : 'video/mp4';
         const headers = {
--- manager.js
+++ manager.js
@@ -10,14 +10,14 @@ const linkStart = process.env.LINKSTART;
 const streamsInfo = new Map();
 const oldChunkSize = 8 * 1024 ** 2;
 
-async function getStreamInfo(id) {
-    if (streamsInfo.has(id)) {
-        return streamsInfo.get(id);
+async function getStreamInfo(fid, cid) {
+    if (streamsInfo.has(fid)) {
+        return streamsInfo.get(fid);
     } else {
         try {
-            const res = await axios.get(`${linkStart}/${id}/blob`);
+            const res = await axios.get(`${linkStart}/${cid}/${fid}/blob`);
             if (!res.data.chunks || !res.data.size) return null;
-            streamsInfo.set(id, res.data);
+            streamsInfo.set(fid, res.data);
             if (streamsInfo.size > maxStreamInfoSize) {
                 streamsInfo.delete(streamsInfo.keys().next().value);
             }
@@ -29,42 +29,42 @@ async function getStreamInfo(id) {
     }
 }
 
-export async function getStreamBuffer(id, start) {
-    const streamInfo = await getStreamInfo(id);
+export async function getStreamBuffer(fid, cid, start) {
+    const streamInfo = await getStreamInfo(fid, cid);
     let chunkSize = streamInfo.chunkSize ? streamInfo.chunkSize : oldChunkSize;
     if (streamInfo === null) return null;
     if (start < 0) return null;
     const index = Math.floor(start / chunkSize);
     if (index >= streamInfo.chunks.length) return null;
-
-    AddToQueue(id, index, streamInfo);
+    AddToQueue(fid, cid, index, streamInfo);
     if (index + 1 < streamInfo.chunks.length) // buffer up more stream
-        AddToQueue(id, index + 1, streamInfo);
-
+        AddToQueue(fid, cid, index + 1, streamInfo);
+    const item = await getFromQueue(fid, index);
+    if(item === undefined) return null;
     return {
-        buffer: (await getFromQueue(id, index)).data,
+        buffer: item.data,
         streamSize: streamInfo.size,
-        chunkSize: streamInfo.chunkSize ? streamInfo.chunkSize : oldChunkSize
+        chunkSize
     };
 }
 
 let Queue = [];
-function AddToQueue(id, index, streamInfo) {
-    const item = Queue.find(w => w.id === id && w.index === index);
+function AddToQueue(fid, cid, index, streamInfo) {
+    const item = Queue.find(w => w.id === fid && w.index === index);
     if (item) return;
     const controller = new AbortController();
-    const buffer = axios.get(`${linkStart}/${streamInfo.chunks[index]}/blob`, {
+    const buffer = axios.get(`${linkStart}/${cid}/${streamInfo.chunks[index]}/blob`, {
         responseType: 'arraybuffer',
         signal: controller.signal
     }).catch(err => { console.log("canceled"); });
-    Queue.unshift({ id, index, buffer, controller });
+    Queue.unshift({ fid, index, buffer, controller });
     if (Queue.length > maxQueueSize) {
         Queue.pop().controller.abort();
     }
 }
 
 async function getFromQueue(id, index) {
-    const item = Queue.find(w => w.id === id && w.index === index);
+    const item = Queue.find(w => w.fid === id && w.index === index);
     if (!item) return null;
     const buffer = await item.buffer;
     return buffer;
